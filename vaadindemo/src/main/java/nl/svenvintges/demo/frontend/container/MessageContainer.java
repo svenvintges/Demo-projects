11fae5e227f7dc9fc87ba6bd8ed70133d074e1d5
Sven Vintges <sven@vintges.org>
2011-04-01 21:47:48 +0200

    test

--- vaadindemo/src/main/java/nl/svenvintges/demo/frontend/container/MessageContainer.java
+++ vaadindemo/src/main/java/nl/svenvintges/demo/frontend/container/MessageContainer.java
@@ -21,6 +21,10 @@ Serializable{
 	 */
 	private static final long serialVersionUID = 4606463452129736939L;
 	
+	/**
+	 * Populates the container based on a given list. Going to find out how it works with hibernate controlled lists ;)
+	 * @param messages
+	 */
 	public void populateContainer(List<Message> messages)
 	{
 		for (Message message : messages) {
